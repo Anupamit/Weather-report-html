3c6b0cc2c34c425a38dcd318d6833dcd4c803875
Anupam kumar <anupamit011@gmail.com>
2023-03-04 13:07:50 +0530

    All files are added

---
+++ Script.js
@@ -0,0 +1,20 @@
+console.log("Hey i am js");
+
+let searchCity=document.getElementById("searchCity");
+let cityName=document.getElementById("cityName");
+let temp=document.getElementById("temp");
+
+let search = async()=>{
+    let inputValue=searchCity.value
+    if (inputValue == "") {
+        alert("Please enter city name before search");//error message
+    } else {
+        let url=`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&units=metric&appid=1cb6532aea3c298a830a71380eace21e`
+        let response = await fetch(url)
+        let data =await response.json()
+        cityName.textContent=`${data.name} , ${data.sys.country}`
+        temp.textContent=data.main.temp
+        console.log(data);
+    }
+    
+}
