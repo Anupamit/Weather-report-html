62af188d86968b90edcd6fdfec37c309dfac82a8
Anupam kumar <anupamit011@gmail.com>
2023-03-06 13:15:21 +0530

    added condition for not found

--- Script.js
+++ Script.js
@@ -10,9 +10,16 @@ let search = async()=>{
         alert("Please enter city name before search");//error message
     } else {
         let url=`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&units=metric&appid=1cb6532aea3c298a830a71380eace21e`
+        console.log(url);
         let response = await fetch(url)
         let data =await response.json()
-        cityName.textContent=`${data.name} , ${data.sys.country}`
+
+        if (data.name) {
+            cityName.textContent=`${data.name} , ${data.sys.country}`
+        }else{
+            cityName.textContent = "Not Found / You have written wrong CityName"
+        }
+        
         temp.textContent=data.main.temp
         console.log(data);
     }
